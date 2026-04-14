a742144d3f8bf0d19dd8be7e988ce1c9d609fcb1
ErrorInshanu <shanusingh21217shanu@gmail.com>
2026-04-15 02:55:47 +0530

    Initial commit - ServeEase app

---
+++ app/(drawer)/(tabs)/index.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+<Image
+  source={require('../../../assets/images/service2.png')}
+  style={{ width: 350, height: 250, marginBottom: 120,alignItems: 'center' }}
+  resizeMode="contain"
+/>
+      <Text style={styles.title}>Welcome </Text>
+
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+  <Text style={{ textAlign: 'center', marginTop: 15 }}>
+  Don’t have an account? 
+  <Text style={{ color: '#4CAF50', fontWeight: 'bold' }}>
+    {' '}Sign up
+  </Text>
+</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  signupText: {
+    marginTop: 20,
+    textAlign: 'center',
+    color: '#555',
+  },
+});
